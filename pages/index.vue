e936e368859b14dccbe33d373600fe9be29d63f9
zi-yu-huang <ziyuhuang1007@gmail.com>
2023-07-03 00:45:46 +0800

    profile component

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
-<template>
-  <Tutorial/>
+<template lang="pug">
+//- 請填寫功能描述👈
+#IndexPage
+  EditPassword
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  components: {
+    EditPassword: () => import("@/components/profile/EditPassword"),
+  },
+  data () {
+    return {};
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+// 排版
+#IndexPage {
+}
+// 元件
+#IndexPage {
+}
+</style>
